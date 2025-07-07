28347cfbcf7720326654dfe50c91a4102a7ca14e
meppega <{ID}+{username}@users.noreply.github.com>
2025-07-07 18:17:25 +0200

    project reorganization, added support for dregsy

---
+++ scripts/sync-helm.sh
@@ -0,0 +1,8 @@
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo update
+# helm pull bitnami/nginx --version 15.14.0
+# helm push nginx-15.14.0.tgz oci://localhost:5000/
+
+# # test
+# helm pull oci://localhost:5000/charts/nginx
+# helm pull oci://localhost:5000/charts/nginx --version 15.14.0
--- scripts/sync-skopeo.sh
+++ scripts/sync-skopeo.sh
@@ -12,4 +12,9 @@ skopeo copy docker://docker.io/library/alpine:3.16 \
     docker://registry:5000/alpine:3.16 \
     --dest-tls-verify=false
 
+# this obiously doesnt work, but it can, convert helm chart to OCI and then synchronize it
+# skopeo copy oci://charts.bitnami.com/bitnami/nginx:15.14.0 \
+#     oci://registry:5000/nginx:15.14 \
+#     --dest-tls-verify=false
+
 echo "Done."
