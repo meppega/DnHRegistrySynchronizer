35beb9e8eda61ca89e7944114c25b232afb2ade3
meppega <{ID}+{username}@users.noreply.github.com>
2025-07-09 17:38:26 +0200

    added SSL and basic auth for local registry for testing

--- scripts/sync-helm.sh
+++ scripts/sync-helm.sh
@@ -1,7 +1,7 @@
 # helm repo add bitnami https://charts.bitnami.com/bitnami
 # helm repo update
 # helm pull bitnami/nginx --version 15.14.0
-# helm push nginx-15.14.0.tgz oci://localhost:5000/
+# helm push nginx-15.14.0.tgz oci://localhost:5000/charts/
 
 # # test
 # helm pull oci://localhost:5000/charts/nginx
--- scripts/sync-skopeo.sh
+++ scripts/sync-skopeo.sh
@@ -2,24 +2,40 @@
 
 set -e
 
+REGISTRY_URL="localhost:5000" # equivalent to "registry:5000"
+REGISTRY_USER="admin"
+REGISTRY_PASS="admin"
+
+echo "$REGISTRY_PASS" | skopeo login "$REGISTRY_URL" --username "$REGISTRY_USER" --password-stdin
+
 echo "Syncing Docker images to registry..."
 
 skopeo copy \
     docker://docker.io/library/alpine:3.22 \
-    docker://registry:5000/alpine:3.22 \
-    --dest-tls-verify=false
+    docker://"$REGISTRY_URL"/alpine:3.22 \
+    # --dest-tls-verify=false
 skopeo copy docker://docker.io/library/alpine:3.16 \
-    docker://registry:5000/alpine:3.16 \
-    --dest-tls-verify=false
+    docker://"$REGISTRY_URL"/alpine:3.16 \
+    # --dest-tls-verify=false
 
 # for reference, bitnami distributes HELM CHARTS, ONLY
 # skopeo inspect docker://charts.bitnami.com/bitnami/nginx:15.14.0
-# use docker's hubs instead
-skopeo inspect docker://docker.io/grafana/grafana:12.0.2
 
-# this obiously doesnt work, but it can, convert helm chart to OCI and then synchronize it
+# use docker's hubs instead
+skopeo copy \
+    docker://docker.io/grafana/grafana:12.0.2 \
+    docker://"$REGISTRY_URL"/charts/grafana:12.0.2 \
+    # --dest-tls-verify=false
+
+skopeo copy \
+    docker://kubernetes.github.io/ingress-nginx:4.13.0 \
+    docker://"$REGISTRY_URL"/charts/ingress-nginx:4.13.0 \
+    # --dest-tls-verify=false
+
+# 
 # skopeo copy oci://charts.bitnami.com/bitnami/nginx:15.14.0 \
-#     oci://registry:5000/nginx:15.14 \
+#     oci://"$REGISTRY_URL"/nginx:15.14 \
 #     --dest-tls-verify=false
+
 sleep 10
 echo "Done."
