a0a929e462211a812885ba4acac14f9f244accc8
meppega <{ID}+{username}@users.noreply.github.com>
2025-07-09 13:34:26 +0200

    updated .gitattributes for qol and skopeo script for testing

--- scripts/sync-skopeo.sh
+++ scripts/sync-skopeo.sh
@@ -12,10 +12,14 @@ skopeo copy docker://docker.io/library/alpine:3.16 \
     docker://registry:5000/alpine:3.16 \
     --dest-tls-verify=false
 
-skopeo inspect oci://charts.bitnami.com/bitnami/nginx:15.14.0
+# for reference, bitnami distributes HELM CHARTS, ONLY
+# skopeo inspect docker://charts.bitnami.com/bitnami/nginx:15.14.0
+# use docker's hubs instead
+skopeo inspect docker://docker.io/grafana/grafana:12.0.2
+
 # this obiously doesnt work, but it can, convert helm chart to OCI and then synchronize it
 # skopeo copy oci://charts.bitnami.com/bitnami/nginx:15.14.0 \
 #     oci://registry:5000/nginx:15.14 \
 #     --dest-tls-verify=false
-
+sleep 10
 echo "Done."
