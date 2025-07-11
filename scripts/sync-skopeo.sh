63650b1acfd1673a4d61350fd1d2a915ece55a03
meppega <{ID}+{username}@users.noreply.github.com>
2025-07-11 13:54:33 +0200

    made HTTPS not require authentication

--- scripts/sync-skopeo.sh
+++ scripts/sync-skopeo.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-REGISTRY_URL="localhost:5000" # equivalent to "registry:5000"
+REGISTRY_URL="registry:5000" # equivalent to "registry:5000"
 REGISTRY_USER="admin"
 REGISTRY_PASS="admin"
 
-echo "$REGISTRY_PASS" | skopeo login "$REGISTRY_URL" --username "$REGISTRY_USER" --password-stdin
+#echo "$REGISTRY_PASS" | skopeo login "$REGISTRY_URL" --username "$REGISTRY_USER" --password-stdin
 
 echo "Syncing Docker images to registry..."
 
 skopeo copy \
     docker://docker.io/library/alpine:3.22 \
     docker://"$REGISTRY_URL"/alpine:3.22 \
-    # --dest-tls-verify=false
+     --dest-tls-verify=false
 skopeo copy docker://docker.io/library/alpine:3.16 \
     docker://"$REGISTRY_URL"/alpine:3.16 \
-    # --dest-tls-verify=false
+     --dest-tls-verify=false
 
 # for reference, bitnami distributes HELM CHARTS, ONLY
 # skopeo inspect docker://charts.bitnami.com/bitnami/nginx:15.14.0
@@ -25,12 +25,13 @@ skopeo copy docker://docker.io/library/alpine:3.16 \
 skopeo copy \
     docker://docker.io/grafana/grafana:12.0.2 \
     docker://"$REGISTRY_URL"/charts/grafana:12.0.2 \
-    # --dest-tls-verify=false
+     --dest-tls-verify=false
 
+# this wont work, the origin is a chart, its here for testing
 skopeo copy \
     docker://kubernetes.github.io/ingress-nginx:4.13.0 \
     docker://"$REGISTRY_URL"/charts/ingress-nginx:4.13.0 \
-    # --dest-tls-verify=false
+     --dest-tls-verify=false
 
 # 
 # skopeo copy oci://charts.bitnami.com/bitnami/nginx:15.14.0 \
