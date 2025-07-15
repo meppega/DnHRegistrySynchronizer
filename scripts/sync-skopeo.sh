591b75114de7770da082b3f225acaee8f2621dd9
meppega <{ID}+{username}@users.noreply.github.com>
2025-07-15 15:18:18 +0200

    UPDATED sync-skopeo.sh, helm features updated and functional

--- scripts/sync-skopeo.sh
+++ scripts/sync-skopeo.sh
@@ -6,53 +6,55 @@ REGISTRY_URL="registry:5000"
 REGISTRY_USER="admin"
 REGISTRY_PASS="admin"
 
-HELM_CHART_REPO_NAME="bitnami" #"kubernetes-ingress"
-HELM_CHART_REPO_URL="https://charts.bitnami.com/bitnami" #"https://kubernetes.github.io/ingress-nginx"
-HELM_CHART_NAME="nginx"
-HELM_CHART_VERSION="15.14.0" #"4.13.0"
-HELM_CHART="${HELM_CHART_NAME}-${HELM_CHART_VERSION}"
-
 #TODO: add comparing if images are different
-check_and_copy_image() {
+check_and_copy_helm() {
+    REPO_NAME="$1"
+    REPO_URL="$2"
+    CHART_NAME="$3"
+    CHART_VERSION="$4"
+    DEST_REPO="$5"
+    FILE_NAME="${CHART_NAME}-${CHART_VERSION}"
+
+    if helm show chart "${DEST_REPO}${CHART_NAME}" --plain-http > /dev/null 2>&1; then
+        echo "Chart ${DEST_REPO}${CHART_NAME} exists. Skipping."
+        return
+    fi
+
+    echo "Chart does not exist. Adding ${DEST_REPO}${CHART_NAME}"
+
+    #echo "  > Adding Helm repository: ${REPO_NAME} ..."
+    #helm repo add "${REPO_NAME}" "${REPO_URL}" --force-update
+    #helm repo update
+
+    echo "  > Pulling Helm chart from traditional repository..."
+    helm pull "${CHART_NAME}" --repo "${REPO_URL}" --version "${CHART_VERSION}"
+    #--destination /data
+
+    if [ ! -f "${FILE_NAME}.tgz" ]; then
+        echo "Error: Chart ${HELM_CHART}.tgz failed to download. Skipping."
+        return
+    fi
+ 
+    helm push \
+    "${FILE_NAME}.tgz" \
+    "${DEST_REPO}" \
+    --plain-http
+
+    rm "${FILE_NAME}.tgz"
+}
+
+check_and_copy_skopeo() {
     SOURCE_IMAGE="$1"
     DEST_IMAGE="$2"
+
     echo "Checking if $DEST_IMAGE exists in registry..."
     if skopeo inspect "docker://${DEST_IMAGE}" --tls-verify=false >/dev/null 2>&1; then
         echo "$DEST_IMAGE already exists. Skipping copy."
-    else
-        echo "$DEST_IMAGE not found. Copying..."
-        skopeo copy "docker://${SOURCE_IMAGE}" "docker://${DEST_IMAGE}" --dest-tls-verify=false
+        return
     fi
-}
 
-#TODO: add comparing if images are different, finish making this
-check_and_copy_chart() {
-    SOURCE_IMAGE="$1"
-    DEST_IMAGE="$2"
-    echo "Checking if $DEST_IMAGE exists in registry..."
-    # if skopeo inspect "oci://${DEST_IMAGE}" --tls-verify=false >/dev/null 2>&1; then
-    if helm pull "oci://registry:5000/charts/ingress-nginx" --version 4.13.0 --destination /tmp --insecure-skip-tls-verify >/dev/null 2>&1; then
-        echo "$DEST_IMAGE already exists. Skipping copy."
-    else
-        echo "$DEST_IMAGE not found. Copying..."
-        helm push \
-            "${SOURCE_IMAGE}" \
-            "oci://${REGISTRY_URL}/charts/" #\
-            #--insecure-skip-tls-verify
-    fi
-}
-
-
-CHART="oci://registry:5000/charts/nginx"
-check_chart() {
-    if helm show chart "$CHART" --plain-http > /dev/null 2>&1; then
-        echo "Chart exists."
-    else
-        echo "Chart does not exist. ${CHART}"
-        # Example alternative action
-        echo "Please push the chart to the registry first."
-    fi
-    #helm show chart "$CHART" --plain-http
+    echo "$DEST_IMAGE not found. Copying..."
+    skopeo copy "docker://${SOURCE_IMAGE}" "docker://${DEST_IMAGE}" --dest-tls-verify=false
 }
 
 # logging skopeo in
@@ -60,32 +62,21 @@ check_chart() {
 
 echo "Syncing Docker images to registry..."
 
-check_and_copy_image "docker.io/library/alpine:3.22" "$REGISTRY_URL/alpine:3.22"
-check_and_copy_image "docker.io/library/alpine:3.16" "$REGISTRY_URL/alpine:3.16"
-check_and_copy_image "docker.io/grafana/grafana:12.0.2" "$REGISTRY_URL/charts/grafana:12.0.2"
+check_and_copy_skopeo "docker.io/library/alpine:3.22" "$REGISTRY_URL/alpine:3.22"
+check_and_copy_skopeo "docker.io/library/alpine:3.16" "$REGISTRY_URL/alpine:3.16"
+check_and_copy_skopeo "docker.io/grafana/grafana:12.0.2" "$REGISTRY_URL/charts/grafana:12.0.2"
 
-# helm chart conversion
-echo "  > Adding traditional Helm repository..."
-#helm repo add "${HELM_CHART_REPO_NAME}" "${HELM_CHART_REPO_URL}" --force-update
-#helm repo update
+check_and_copy_helm "bitnami" \
+    "https://charts.bitnami.com/bitnami" \
+    "nginx" \
+    "15.14.0" \
+    "oci://registry:5000/charts/"
 
-echo "  > Pulling Helm chart from traditional repository..."
-#helm pull "${HELM_CHART_REPO_NAME}/${HELM_CHART_NAME}" --version "${HELM_CHART_VERSION}"
-#--destination /data
-check_chart
+check_and_copy_helm "kubernetes-ingress" \
+    "https://kubernetes.github.io/ingress-nginx" \
+    "ingress-nginx" \
+    "4.13.0" \
+    "oci://registry:5000/charts/"
 
-if [ -f "${HELM_CHART}.tgz" ]; then
-    #check_and_copy_chart "${HELM_CHART}.tgz" "${REGISTRY_URL}/charts/${HELM_CHART_NAME}:${HELM_CHART_VERSION}"
-    # helm push \
-    #     "${HELM_CHART_NAME}-${HELM_CHART_VERSION}.tgz" \
-    #     "oci://${REGISTRY_URL}/charts/" \
-    #     --insecure-skip-tls-verify --plain-http
-    rm "${HELM_CHART}.tgz"
-else
-    #helm pull kubernetes-ingress/ingress-nginx --version "${HELM_CHART_VERSION}"
-    #check_chart()
-    echo "Error: Chart ${HELM_CHART}.tgz was not downloaded. Skipping."
-fi
-
-sleep 3
+sleep 1
 echo "Done."
